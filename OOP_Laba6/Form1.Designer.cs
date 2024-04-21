7174e34510f3e46771cdb19e659486558dba030d
ZeroX464 <rasskazov-2004@mail.ru>
2024-04-22 01:12:51 +0500

    Добавьте файлы проекта.

---
+++ OOP_Laba6/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace OOP_Laba6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.squareButton = new System.Windows.Forms.Button();
+            this.circleButton = new System.Windows.Forms.Button();
+            this.triangleButton = new System.Windows.Forms.Button();
+            this.sectionButton = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.cursorButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.InsetDouble;
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.squareButton, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.circleButton, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.triangleButton, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.sectionButton, 0, 3);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 24);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(117, 426);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // squareButton
+            // 
+            this.squareButton.BackgroundImage = global::OOP_Laba6.Properties.Resources.square;
+            this.squareButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.squareButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.squareButton.Location = new System.Drawing.Point(6, 6);
+            this.squareButton.Name = "squareButton";
+            this.squareButton.Size = new System.Drawing.Size(105, 96);
+            this.squareButton.TabIndex = 5;
+            this.squareButton.UseVisualStyleBackColor = true;
+            this.squareButton.MouseClick += new System.Windows.Forms.MouseEventHandler(this.squareButton_MouseClick);
+            // 
+            // circleButton
+            // 
+            this.circleButton.BackgroundImage = global::OOP_Laba6.Properties.Resources.circle;
+            this.circleButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.circleButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.circleButton.Location = new System.Drawing.Point(6, 111);
+            this.circleButton.Name = "circleButton";
+            this.circleButton.Size = new System.Drawing.Size(105, 96);
+            this.circleButton.TabIndex = 6;
+            this.circleButton.UseVisualStyleBackColor = true;
+            this.circleButton.MouseClick += new System.Windows.Forms.MouseEventHandler(this.circleButton_MouseClick);
+            // 
+            // triangleButton
+            // 
+            this.triangleButton.BackgroundImage = global::OOP_Laba6.Properties.Resources.triangle;
+            this.triangleButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.triangleButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.triangleButton.Location = new System.Drawing.Point(6, 216);
+            this.triangleButton.Name = "triangleButton";
+            this.triangleButton.Size = new System.Drawing.Size(105, 96);
+            this.triangleButton.TabIndex = 7;
+            this.triangleButton.UseVisualStyleBackColor = true;
+            this.triangleButton.MouseClick += new System.Windows.Forms.MouseEventHandler(this.triangleButton_MouseClick);
+            // 
+            // sectionButton
+            // 
+            this.sectionButton.BackgroundImage = global::OOP_Laba6.Properties.Resources.section;
+            this.sectionButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.sectionButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.sectionButton.Location = new System.Drawing.Point(6, 321);
+            this.sectionButton.Name = "sectionButton";
+            this.sectionButton.Size = new System.Drawing.Size(105, 99);
+            this.sectionButton.TabIndex = 8;
+            this.sectionButton.UseVisualStyleBackColor = true;
+            this.sectionButton.MouseClick += new System.Windows.Forms.MouseEventHandler(this.sectionButton_MouseClick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(117, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(683, 426);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            // 
+            // cursorButton
+            // 
+            this.cursorButton.BackColor = System.Drawing.SystemColors.Control;
+            this.cursorButton.BackgroundImage = global::OOP_Laba6.Properties.Resources.cursor;
+            this.cursorButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.cursorButton.Location = new System.Drawing.Point(0, 0);
+            this.cursorButton.Name = "cursorButton";
+            this.cursorButton.Size = new System.Drawing.Size(36, 24);
+            this.cursorButton.TabIndex = 4;
+            this.cursorButton.UseVisualStyleBackColor = false;
+            this.cursorButton.MouseClick += new System.Windows.Forms.MouseEventHandler(this.cursorButton_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cursorButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Button cursorButton;
+        private System.Windows.Forms.Button squareButton;
+        private System.Windows.Forms.Button circleButton;
+        private System.Windows.Forms.Button triangleButton;
+        private System.Windows.Forms.Button sectionButton;
+    }
+}
+
---
+++ OOP_Laba6/Form1.cs
@@ -0,0 +1,346 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP_Laba6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        class Figure 
+        {
+            protected int x, y;
+            public bool selected = false;
+            public virtual void DrawMe(Graphics g) {}
+            public virtual bool SelectionCheck(MouseEventArgs e) { return false; }
+            public int getRLength(MouseEventArgs e)
+            {
+                return (int)Math.Sqrt(Math.Pow((e.X - x), 2) + Math.Pow((e.Y - y), 2));
+            }
+            public bool IsSelected()
+            {
+                if (selected)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        class Square : Figure
+        {
+            private const int side = 80;
+            public Square() { x = 0; y = 0; }
+            public Square(int x, int y) { this.x = x; this.y = y; }
+            public Square(Square square) { x = square.x; y = square.y; }
+            ~Square() { }
+            public override void DrawMe(Graphics g)
+            {
+                Pen pen = new Pen(Color.Brown, 3);
+                if (selected)
+                {
+                    g.DrawRectangle(pen, x - side / 2, y - side / 2, side, side);
+                    g.FillRectangle(Brushes.Blue, x - side / 2, y - side / 2, side, side);
+                }
+                else
+                {
+                    g.DrawRectangle(pen, x - side / 2, y - side / 2, side, side);
+                    g.FillRectangle(Brushes.Black, x - side / 2, y - side / 2, side, side);
+                }
+            }
+            public override bool SelectionCheck(MouseEventArgs e)
+            {
+                if ((e.X >= x - side / 2) && (e.X <= x + side / 2) && (e.Y >= y - side / 2) && (e.Y <= y + side / 2)) { return true; }
+                else { return false; }
+            }
+        }
+        class CCircle : Figure
+        {
+            private const int radius = 40;
+            public CCircle() { x = 0; y = 0; }
+            public CCircle(int x, int y) { this.x = x; this.y = y; }
+            public CCircle(CCircle circle) { x = circle.x; y = circle.y; }
+            ~CCircle() { }
+            public override void DrawMe(Graphics g)
+            {
+                Pen pen = new Pen(Color.Brown, 3);
+                if (selected)
+                {
+                    g.DrawEllipse(pen, x - radius, y - radius, radius * 2, radius * 2);
+                    g.FillEllipse(Brushes.Blue, x - radius, y - radius, radius * 2, radius * 2);
+                }
+                else
+                {
+                    g.DrawEllipse(pen, x - radius, y - radius, radius * 2, radius * 2);
+                    g.FillEllipse(Brushes.Black, x - radius, y - radius, radius * 2, radius * 2);
+                } 
+            }
+            public override bool SelectionCheck(MouseEventArgs e)
+            {
+                if ((e.X > x - radius && e.X < x + radius) && (e.Y > y - radius && e.Y < y + radius)) { return true; }
+                else { return false; }
+            }
+        }
+        class Triangle : Figure
+        {
+            private Point[] points;
+            public Triangle() { x = 0; y = 0; points = InitPoints(); }
+            public Triangle(int x, int y) { this.x = x; this.y = y; points = InitPoints(); }
+            public Triangle(Triangle triangle) { x = triangle.x; y = triangle.y; points = InitPoints(); }
+            ~Triangle() { }
+            private Point[] InitPoints()
+            {
+                Point[] points =
+                {
+                    new Point(x + 40, y + 40),
+                    new Point(x - 40, y + 40),
+                    new Point(x, y - 40),
+                };
+                return points;
+        }
+            public override void DrawMe(Graphics g)
+            {
+                Pen pen = new Pen(Color.Brown, 3);
+                if (selected)
+                {
+                    g.DrawPolygon(pen, points);
+                    g.FillPolygon(Brushes.Blue, points);
+                }
+                else
+                {
+                    g.DrawPolygon(pen, points);
+                    g.FillPolygon(Brushes.Black, points);
+                }
+            }
+            public override bool SelectionCheck(MouseEventArgs e)
+            {
+                GraphicsPath path = new GraphicsPath();
+                path.AddPolygon(points);
+                return path.IsVisible(e.Location); // Функция считает кол-во пересечений полигона лучом из заданной точки в случайном направлении
+            }
+        }
+        class Section : Figure
+        {
+            private Point end_point;
+            public Section() { x = 0; y = 0; end_point = new Point(0, 0); }
+            public Section(Point a, Point b) { this.x = a.X; this.y = a.Y; end_point = b; }
+            public Section(Section section) { x = section.x; y = section.y; end_point = section.end_point; }
+            ~Section() {}
+            public override void DrawMe(Graphics g)
+            {
+                Pen pen = new Pen(Color.Black, 100);
+                if (selected)
+                {
+                    pen.Color = Color.Blue;
+                    g.DrawLine(pen, x, y, end_point.X, end_point.Y);
+                }
+                else
+                {
+                    g.DrawLine(pen, x, y, end_point.X, end_point.Y);
+                }
+            }
+            public override bool SelectionCheck(MouseEventArgs e)
+            {
+                if () { return true; } // Здесь должна быть проверка расстояния от точки до прямой
+                else { return false; }
+            }
+        }
+        class container
+        {
+            Figure[] array;
+            private int size, capacity;
+            public container() { size = 0; capacity = 1; array = new Figure[capacity]; }
+            public container(int capacity) { size = 0; this.capacity = capacity; array = new Figure[capacity]; }
+            public container(container c) { size = c.size; capacity = c.capacity; array = new container(c).array; }
+            ~container() { }
+            public void Push_back(Figure element)
+            {
+                if (size >= capacity)
+                {
+                    capacity *= 2;
+                    Figure[] newArr = new Figure[capacity];
+                    for (int i = 0; i < size; i++)
+                    {
+                        newArr[i] = array[i];
+                    }
+                    array = newArr;
+                }
+                array[size++] = element;
+            }
+            public void DrawAll(Graphics g)
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    array[i].DrawMe(g);
+                }
+            }
+            public bool SelectionCheckAll(MouseEventArgs e, bool CtrlPressed, bool MultiplySelections)
+            {
+                bool anyIsSelected = false;
+                if (MultiplySelections)
+                {
+                    for (int i = 0; i < size; i++)
+                    {
+                        if (array[i].SelectionCheck(e) == true) { anyIsSelected = true; }
+                    }
+                }
+                else
+                {
+                    int minRLength = 10000;
+                    Figure nearestObj = null;
+                    for (int i = 0; i < size; i++)
+                    {
+                        if (array[i].SelectionCheck(e) == true)
+                        {
+                            anyIsSelected = true;
+                            if (array[i].getRLength(e) < minRLength)
+                            {
+                                nearestObj = array[i];
+                            }
+
+                        }
+                    }
+                    if (nearestObj != null)
+                    {
+                        if (!CtrlPressed) { MakeAllObjsUnselected(); }
+                        nearestObj.selected = true;
+                    }
+
+                }
+                return anyIsSelected;
+            }
+            public void DeleteSelectedFromContainer()
+            {
+                int const_size = size;
+                for (int i = 0; i < const_size; i++)
+                {
+                    if (array[i] != null)
+                    {
+                        if (array[i].IsSelected())
+                        {
+                            DeleteObjFromContainer(array[i]);
+                            i--;
+                        }
+                    }
+                }
+            }
+            public void DeleteObjFromContainer(Figure FigureToDelete)
+            {
+                Figure[] newArr = new Figure[capacity];
+                bool elemFind = false;
+                size--;
+                for (int i = 0; i < size; i++)
+                {
+                    if (array[i] == FigureToDelete)
+                    {
+                        elemFind = true;
+                    }
+                    if (elemFind) { newArr[i] = array[i + 1]; }
+                    else { newArr[i] = array[i]; }
+                }
+                if (array[size] == FigureToDelete) { elemFind = true; } //Для последнего элемента
+                array = newArr;
+                if (!elemFind) { throw new Exception("ObjNotFoundInContainer"); }
+            }
+            public void MakeLastObjSelected()
+            {
+                if (size > 0)
+                {
+                    array[size - 1].selected = true;
+                }
+            }
+            public void MakeAllObjsUnselected()
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    array[i].selected = false;
+                }
+            }
+        }
+
+        container c = new container();
+        bool CtrlPressed = false;
+        bool MultiplySelections = false;
+        string figure = null;
+        Point start_point;
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (!c.SelectionCheckAll(e, CtrlPressed, MultiplySelections) && (figure != null))
+            {
+                Figure createdFigure = null;
+                switch (figure)
+                {
+                    case "Square":
+                        createdFigure = new Square(e.X, e.Y);
+                        break;
+                    case "Circle":
+                        createdFigure = new CCircle(e.X, e.Y);
+                        break;
+                    case "Triangle":
+                        createdFigure = new Triangle(e.X, e.Y);
+                        break;
+                    case "Section":
+                        createdFigure = new Section(start_point, e.Location);
+                        break;
+                }
+                if (createdFigure != null)
+                {
+                    c.Push_back(createdFigure);
+                    createdFigure.selected = true;
+                }
+            }
+            this.Refresh();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            c.DrawAll(e.Graphics);
+        }
+
+        private void squareButton_MouseClick(object sender, MouseEventArgs e)
+        {
+            figure = "Square";
+        }
+
+        private void circleButton_MouseClick(object sender, MouseEventArgs e)
+        {
+            figure = "Circle";
+        }
+
+        private void triangleButton_MouseClick(object sender, MouseEventArgs e)
+        {
+            figure = "Triangle";
+        }
+
+        private void sectionButton_MouseClick(object sender, MouseEventArgs e)
+        {
+            figure = "Section";
+        }
+
+        private void cursorButton_MouseClick(object sender, MouseEventArgs e)
+        {
+            figure = null;
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            start_point = e.Location;
+        }
+    }
+}
